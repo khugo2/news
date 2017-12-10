4037455c95145b7e152e702d9c7707a6cf13c991
Hugo Kiiski <hugo.kiiski@hotmail.com>
2017-12-11 01:30:41 +0200

    Remove db configs

--- src/main/java/news/NewsApplication.java
+++ src/main/java/news/NewsApplication.java
@@ -12,21 +12,7 @@ import java.sql.SQLException;
 
 @SpringBootApplication
 public class NewsApplication {
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
-
     public static void main(String[] args) {
         SpringApplication.run(NewsApplication.class, args);
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        if (dbUrl == null || dbUrl.isEmpty()) {
-            return new HikariDataSource();
-        } else {
-            HikariConfig config = new HikariConfig();
-            config.setJdbcUrl(dbUrl);
-            return new HikariDataSource(config);
-        }
     }
 }
