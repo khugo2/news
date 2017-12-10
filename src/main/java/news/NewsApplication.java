6edda24931d0e83879a764bd5d7516a14f7209c1
Hugo Kiiski <hugo.kiiski@hotmail.com>
2017-12-11 01:36:50 +0200

    Heroku deploy with travis

--- src/main/java/news/NewsApplication.java
+++ src/main/java/news/NewsApplication.java
@@ -1,14 +1,7 @@
 package news;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import javax.sql.DataSource;
-import java.sql.SQLException;
 
 @SpringBootApplication
 public class NewsApplication {
